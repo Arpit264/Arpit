05079024a22f17e548584c8ba626e3bf56f1f89a
Arpit264 <92380074+Arpit264@users.noreply.github.com>
2024-10-13 21:37:48 +0530

    Update titan.py

--- titan.py
+++ titan.py
@@ -11,8 +11,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # Inbuilt function to set bot token and admin IDs
 def set_config():
     global TOKEN, ADMIN_IDS
-    TOKEN = "7250145152:AAEjy5oCpZfApyBgVuMQ9e_ogn-dolxxro"  # Replace with your bot token
-    ADMIN_IDS = [7163028849]    # Replace with your admin IDs as integers
+    TOKEN = "7608188361:AAHF9c1qnFLA5YbKucLYKwIdY7ezd9nQuSE"  # Replace with your bot token
+    ADMIN_IDS = [6030543278]    # Replace with your admin IDs as integers
 
 set_config()
 
